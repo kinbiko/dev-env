62f26f765cff9f19385cbdd5c4c2fe8064a0a3b3
Roger Guldbrandsen <roger@kinbiko.com>
2017-01-09 14:45:03 +0000

    Wrote check if root function

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
 #ensure you are running these commands as root
-checkThatYouAreRoot()
-addTheRepositoriesRequiredForTheInstallationOfThePrerequisites()
-ensureBashIsHeartbleedProof()
+#Will exit with an error message if run as as a non-root user
+function checkThatYouAreRoot {
+    if [ "$EUID" -ne 0 ]; then
+        echo "Logged in as root, preparing for development environment installation..."
+    else
+        echo "Please log in as root user."
+        exit -1;
+    fi
+}
 
+#Debian is free as in freedom, and therefore does not support all package
+#repositories:that one may want out of the box
+#This function adds the repositories needed for the installation of 
+function addTheRepositoriesRequiredForTheInstallationOfThePrerequisites{
+
+}
+
+function ensureBashIsHeartbleedProof{
+
+}
+
+checkThatYouAreRoot
 apt-get update
 apt-get upgrade
 
@@ -16,8 +34,13 @@ if(rdp)
 else
 	installVNCServer();
 
-#Should prompt for username w.r.t. ldap server.
-#ldap configurations can be hardcoded in the script
 createUser()
+
+username='ace.ventura'
+
+cd /home/$username
+git clone https://github.com/kinbiko/dev-env
+
+letTheUserBeInTheSudoersList()
 
 echo "The rest of the installation can be performed by the developer"
