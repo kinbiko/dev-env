25c2b151ea4175fe21f1a496f3568937e73f9f90
Roger Guldbrandsen <roger@kinbiko.com>
2017-01-10 10:47:22 +0000

    Minor improvements when installing docker

--- install-docker.sh
+++ install-docker.sh
@@ -2,17 +2,17 @@
 
 ### apt-repository handling ###
 
-sudo apt-get purge "lxc-docker*"
-sudo apt-get purge "docker.io*"
+sudo apt-get purge "lxc-docker*" > /dev/null
+sudo apt-get purge "docker.io*" > /dev/null
 
-sudo apt-get update
+sudo apt-get update > /dev/null
 sudo apt-get install apt-transport-https ca-certificates gnupg2
 
 sudo apt-key adv \
     --keyserver hkp://ha.pool.sks-keyservers.net:80 \
     --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-sudo echo "deb https://apt.dockerproject.org/repo debian-jessie main" >> /etc/apt/sources.list.d/docker.list
+sudo echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list
 
 sudo apt-get update
 
