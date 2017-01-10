4e4922003acfb73e3a78f7080769d07533c18c74
Roger Guldbrandsen <roger@kinbiko.com>
2017-01-10 15:27:14 +0000

    Added subshell trick to storing java deb repos

--- wip/install-java.sh
+++ wip/install-java.sh
@@ -3,8 +3,8 @@
 #The following packages are labelled ubuntu, but as they only contain an installer,
 #it will work on debian as well.
 echo "Storing the deb repositories"
-sudo sh -c 'echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list'
-sudo sh -c 'echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list'
+sudo sh -c 'echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" > /etc/apt/sources.list.d/webupd8team-java.list'
+sudo sh -c 'echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" >> /etc/apt/sources.list.d/webupd8team-java.list'
 
 echo "Saving key for the ubuntu keyserver"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886 > /dev/null
