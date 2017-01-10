2c6f6b8b7f954d5cbfbd0a5b21d990ddab82d207
Roger Guldbrandsen <roger@kinbiko.com>
2017-01-10 16:34:24 +0000

    Added mvn instructions

--- install-maven-3.3.9.sh
+++ install-maven-3.3.9.sh
@@ -12,8 +12,7 @@ sudo mv apache-maven-3.3.9 /opt/
 echo "adding the maven path to the path"
 echo "export PATH=$PATH:/opt/apache-maven-3.3.9/bin/" >> ~/.bashrc
 
-#The following allows the users to use Maven immediately
-export PATH=$PATH:/opt/apache-maven-3.3.9/bin
-
 echo "Removing tarball"
 rm apache-maven-3.3.9-bin.tar.gz
+
+echo "Installation complete. Start a new shell to use Maven"
