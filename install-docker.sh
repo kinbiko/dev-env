a30b4b5637bedbedeb83f6daa5e2eac133b91dfd
Roger Guldbrandsen <roger@kinbiko.com>
2017-01-10 10:40:10 +0000

    Use permissions in writing to sources.list.d

--- install-docker.sh
+++ install-docker.sh
@@ -12,9 +12,9 @@ sudo apt-key adv \
     --keyserver hkp://ha.pool.sks-keyservers.net:80 \
     --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-echo "deb https://apt.dockerproject.org/repo debian-jessie main" >> /etc/apt/sources.list.d/docker.list
+sudo echo "deb https://apt.dockerproject.org/repo debian-jessie main" >> /etc/apt/sources.list.d/docker.list
 
 sudo apt-get update
 
 ### Installation ###
-sudo apt-get install docker-engine
+sudo apt-get -y install docker-engine
