a355fcb9cc562b603292bbd9991b3be1747fe901
Roger Guldbrandsen <roger@kinbiko.com>
2017-01-10 15:10:29 +0000

    added install-java.sh

---
+++ install-java.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#The following packages are labelled ubuntu, but as they only contain an installer,
+#it will work on debian as well.
+echo "Storing the deb repositories"
+sudo echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list > /dev/null
+sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list > /dev/null
+
+echo "Saving key for the ubuntu keyserver"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886 > /dev/null
+echo "Updating package list to find Oracle Java"
+sudo apt-get update > /dev/null
+echo "Downloading and starting installer."
+sudo apt-get install oracle-java8-installer
+echo "Java compiler version:"
+javac -version
+echo "Java version:"
+java -version
+echo "Installation complete"
