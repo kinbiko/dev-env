913f3fe5f248fc5fdff71890e5c1a5c496aebf0f
Roger Guldbrandsen <roger@kinbiko.com>
2017-01-11 22:17:13 +0000

    Added install script for Vim 8

---
+++ extras/install-vim-8.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Installing build VIM dependencies"
+sudo apt-get -y install gcc ncurses
+
+echo "Downloading source code..."
+cd ~
+wget https://github.com/vim/vim/archive/master.zip
+unzip master.zip
+
+cd vim-master/src/
+echo "Configuring VIM build..."
+./configure > /dev/null
+echo "Building VIM..."
+make > /dev/null
+echo "Installing VIM..."
+sudo make install > /dev/null
+
+echo "Removing temporary files..."
+cd ~
+rm master.zip
+
+echo "Most recent version of VIM is now installed"
