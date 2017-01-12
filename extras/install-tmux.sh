183a944f2624fd6b24154ed815607fccd4ff0e08
Roger Guldbrandsen <roger@kinbiko.com>
2017-01-12 23:07:40 +0000

    Agnostified tmux install

--- extras/install-tmux.sh
+++ extras/install-tmux.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+read -p "Distro ('debian' or 'ubuntu'): " distro
+
 #The version in the repository is < 2.x
 VERSION=2.3
 
@@ -14,7 +16,13 @@ cd tmux-$VERSION
 #Install the dependencies for building tmux from source
 echo "Installing Tmux build dependencies"
 #for some reason 'sudo apt-get build-dep tmux' doesn't work...
-sudo apt-get -y install libevent-1.4 libevent-dev libncurses5-dev
+
+if [ "$distro" = "debian" ]; then
+    sudo apt-get -y build-dep tmux
+else
+    sudo apt-get -y install libevent-1.4 libevent-dev libncurses5-dev
+fi
+
 
 echo "Configuring the build..."
 ./configure --prefix=/usr
