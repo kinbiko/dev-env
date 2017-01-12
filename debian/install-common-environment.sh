c49a012698c3d2c1fc1a0911060686b39763b5fc
Roger Guldbrandsen <roger@kinbiko.com>
2017-01-12 22:27:41 +0000

    Made install-common-environment ask for distro

--- debian/install-common-environment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-("../agnostic/configure-user-system.sh")
-
-("./common/install-docker.sh")
-("../agnostic/common/install-chrome.sh")
-("../agnostic/common/install-git.sh")
-
-echo "Docker, Google-Chrome and Git is now installed."
---
+++ install-common-environment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Distro ('debian' or 'ubuntu'): " distro
+
+("./agnostic/configure-user-system.sh")
+
+("./$distro/common/install-docker.sh")
+("./agnostic/common/install-chrome.sh")
+("./agnostic/common/install-git.sh")
+
+echo "Docker, Google-Chrome and Git are now installed."
